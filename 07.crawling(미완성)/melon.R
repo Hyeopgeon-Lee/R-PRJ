ab5ccc091906b5d58af78ae6cd78e16c07f14e8a
Hyeopgeon Lee <soft1318@naver.com>
2024-09-04 10:55:37 +0900

    웹 클로링소스 수정

--- 07.crawling(미완성)/melon.R
+++ 07.crawling(미완성)/melon.R
@@ -1,35 +1,51 @@
 # 필요한 패키지 설치
 install.packages("rvest")
 install.packages("dplyr")
+install.packages("curl")
 
 # 패키지 로드
 library(rvest)
 library(dplyr)
+library(curl)
 
 # 멜론 차트 URL
-url <-  <- read_html(curl('http://benchmarkrealestate.com/', handle = new_handle("useragent" = "Mozilla/5.0")))
+url <- "https://www.melon.com/chart/index.htm"
 
 # 웹페이지 읽기
-webpage <- read_html(url)
+webpage <- read_html(curl(url, handle = new_handle("useragent" = "Mozilla/5.0")))
 
-# 순위 추출
+# 순위 추출 (실제 페이지 구조에 맞는 선택자를 사용해야 함)
 rank <- webpage %>%
-  html_nodes('span.rank') %>%
+  html_nodes('tbody span.rank') %>%  # 실제 CSS 선택자에 맞게 수정
   html_text() %>%
   as.numeric()
+
+rank
 
 # 노래 제목 추출
 title <- webpage %>%
   html_nodes('div.ellipsis.rank01 a') %>%
   html_text()
 
-# 가수 이름 추출
+title
+
+# 가수 이름 결합
 artist <- webpage %>%
-  html_nodes('div.ellipsis.rank02 a') %>%
-  html_text()
+  html_nodes('div.ellipsis.rank02 span.checkEllipsis') %>%
+  lapply(function(node) {
+    node %>%
+      html_nodes('a') %>%
+      html_text() %>%
+      paste(collapse = ", ")  # 쉼표로 결합하여 하나의 문자열로 처리
+  }) %>%
+  unlist()
+
+artist
+
 
 # 데이터 프레임으로 저장
-melon_chart <- data.frame(순위 = rank, 노래 = title, 가수 = artist)
+melon_chart <- data.frame(순위 = rank, 노래 = title, 가수 = artist, stringsAsFactors = FALSE)
 
 # 결과 확인
-print(melon_chart)
+View(melon_chart)
+
